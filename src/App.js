2cc690ef78869972436e948371f14a8041fa9375
Mohd Sameer <sa5802257@gmail.com>
2024-04-19 10:24:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
+
+
+                  // this was a old method 
+
+// const routeOldMethod = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeOldMethod)
+
+              
+              // NEW APPROACH FOR CREATING A ROUTER
+
+// createBrowserRouter is a inbuilt packages with the help of that we can create router
+const router = createBrowserRouter([
+  {
+    // Rooot Layout will consider as Parent router 
+    path : '/',
+    element : <RootLayout />,
+    errorElement: <ErrorPage />,
+
+    // inside children array which router present consider as a child router of the parent router
+    children : [
+      // setting all the path are the relative path 
+      { path: '', element: <HomePage /> },
+      { path: 'products', element: <ProductsPage />},
+      { path: 'products/:productId', element: <ProductDetailPage /> }
+    ]
+  },
+  
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
